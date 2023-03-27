261127ba4dabf6e42154205b7f1c7c274151119e
Ben <ben.kohlenberg@web.de>
2023-03-27 14:48:08 +0200

    Tabeinlesen

--- Test-Datei.py
+++ Test-Datei.py
@@ -1,18 +1,23 @@
 #Hier soll die Hello world Datei ausgeführt werden.
-x = 0
+import os
 #tabelle erstellen nicht öffnen
-f = open("Tabelle.txt", "r")
+def appendtab(mdn,name,pos,dauer):
+    f = open("Tabelle.txt", "a")
+    f.write(mdn + "\t" + name + "\t" + pos + "\t" + dauer + "\n")
+    f.close
+os.remove("Tabelle.txt")    
+appendtab("1","Git","2,3","20")
+appendtab("2","Py","0","5")
+appendtab("3","Sps","0","30")
+
 #abfrage des Ziellernmoduls
-print ("In welchem Lernbereich sind Sie aktuell? ")
-#
-input (x)
-if(x == 1):
-    print("Du hast bereits 20 Stunden geschafft. ")
-elif(x==3):
-    print("du hast bereits 30 Stunden geschafft. ")
-else:
-    print("Du hast bereits 25 Stunden geschafft.")
+# if(x == 1):
+#     print("Du hast bereits 20 Stunden geschafft. ")
+# elif(x==3):
+#     print("du hast bereits 30 Stunden geschafft. ")
+# else:
+#     print("Du hast bereits 25 Stunden geschafft.")
 input ("")
-b = open (Hello_World())
-f.close
-#
+# b = open (Hello_World())
+
+
