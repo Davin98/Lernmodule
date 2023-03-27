a9dcce7a3a9cb159539fb6d6ccbf636a591b8537
Ben <ben.kohlenberg@web.de>
2023-03-27 13:36:51 +0200

    Hallo

--- Test-Datei.py
+++ Test-Datei.py
@@ -1,6 +1,18 @@
 #Hier soll die Hello world Datei ausgeführt werden.
+x = 0
+#tabelle erstellen nicht öffnen
 f = open("Tabelle.txt", "r")
-print (f.read())
+#abfrage des Ziellernmoduls
+print ("In welchem Lernbereich sind Sie aktuell? ")
+#
+input (x)
+if(x == 1):
+    print("Du hast bereits 20 Stunden geschafft. ")
+elif(x==3):
+    print("du hast bereits 30 Stunden geschafft. ")
+else:
+    print("Du hast bereits 25 Stunden geschafft.")
 input ("")
+b = open (Hello_World())
 f.close
-
+#
