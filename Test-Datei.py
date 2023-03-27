30388a6d2d56791989ad9017d684ff6900c2dd9c
Ben <ben.kohlenberg@web.de>
2023-03-27 15:59:56 +0200

    Hallo

--- Test-Datei.py
+++ Test-Datei.py
@@ -1,23 +1,19 @@
 #Hier soll die Hello world Datei ausgeführt werden.
 import os
-#tabelle erstellen nicht öffnen
+#tabelle wird erstellt und zeilen eingetragen
 def appendtab(mdn,name,pos,dauer):
     f = open("Tabelle.txt", "a")
     f.write(mdn + "\t" + name + "\t" + pos + "\t" + dauer + "\n")
     f.close
-os.remove("Tabelle.txt")    
+#die Datei "Tabelle.txt" wird gelöscht
+os.remove("Tabelle.txt")  
+#in der Datei Tabelle.txt werden die folgenden zeilen eingetragen  
 appendtab("1","Git","2,3","20")
 appendtab("2","Py","0","5")
 appendtab("3","Sps","0","30")
+#einfacher input befehl, sodass das programm nicht sofort beendet wird
 
-#abfrage des Ziellernmoduls
-# if(x == 1):
-#     print("Du hast bereits 20 Stunden geschafft. ")
-# elif(x==3):
-#     print("du hast bereits 30 Stunden geschafft. ")
-# else:
-#     print("Du hast bereits 25 Stunden geschafft.")
 input ("")
-# b = open (Hello_World())
+
 
 
